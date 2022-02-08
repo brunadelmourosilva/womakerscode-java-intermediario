7ac566cc168969c32719c971fe4d077bd422c9bb
Bruna Delmouro <brunadelmouro@gmail.com>
2022-02-08 15:15:17 -0300

    aula 59

---
+++ src/com/bruna/javaintermediario/elementos_basicos/escopo_var/Aula60.java
@@ -0,0 +1,18 @@
+package com.bruna.javaintermediario.elementos_basicos.escopo_var;
+
+public class Aula60 {
+    public static void main(String[] args) {
+        EscopoVariaveis escopo = new EscopoVariaveis();
+        escopo.setValor(10);
+
+        System.out.println(escopo.getValor()); //10
+
+        System.out.println(escopo.calculaValor(20)); //20 ou 30? : 30
+
+        System.out.println(escopo.getValor()); //10 ou 20?
+
+        System.out.println(escopo.teste()); //4 ou 9
+
+        System.out.println(escopo.getValor());
+    }
+}
--- src/com/bruna/javaintermediario/elementos_basicos/import_static/Aula59.java
+++ src/com/bruna/javaintermediario/elementos_basicos/import_static/Aula59.java
@@ -1,11 +1,10 @@
-package com.bruna.javaintermediario.elementos_basicos.wrapper;
+package com.bruna.javaintermediario.elementos_basicos.import_static;
 
 //static import
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 public class Aula59 {
-
     public static void main(String[] args) {
 
         double a = 2;
@@ -21,3 +20,6 @@ public class Aula59 {
         System.out.println(sqrt(c));
     }
 }
+
+
+
