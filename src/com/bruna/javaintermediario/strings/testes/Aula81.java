2a4cdd5ce9184a24671d85fb6cb06f637b8c820d
Bruna Delmouro <brunadelmouro@gmail.com>
2022-02-13 12:50:58 -0300

    aula 81

---
+++ src/com/bruna/javaintermediario/strings/testes/Aula81.java
@@ -0,0 +1,18 @@
+package com.bruna.javaintermediario.strings.testes;
+
+public class Aula81 {
+
+    public static void main(String[] args) {
+
+        String teste = "Teste";
+
+        String testeMinusc = teste.toLowerCase();
+        String testeMaisc = teste.toUpperCase();
+
+        System.out.println(testeMinusc);
+        System.out.println(testeMaisc);
+
+        //comparação
+        //if (teste.toLowerCase().equals(teste.toLowerCase()))
+    }
+}
