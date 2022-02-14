c42d255ada613228e7286d3c6feca51782a18bf6
Bruna Delmouro <brunadelmouro@gmail.com>
2022-02-14 20:27:07 -0300

    aula 85

---
+++ src/com/bruna/javaintermediario/classes_utilitarias/testes/Aula85.java
@@ -0,0 +1,32 @@
+package com.bruna.javaintermediario.classes_utilitarias.testes;
+
+public class Aula85 {
+
+    public static void main(String[] args) {
+
+        //potência
+        System.out.println(Math.pow(2, 3));
+
+        //arredonda para 5
+        System.out.println(Math.round(4.7));
+
+        //arredonda para 4
+        System.out.println(Math.round(4.4));
+
+        //arredonda para cima
+        System.out.println(Math.ceil(4.4));
+        System.out.println(Math.ceil(4.7));
+
+        //arredonda para baixo
+        System.out.println(Math.floor(4.4));
+        System.out.println(Math.floor(4.7));
+
+        //gera um número aleatório de 0 a 100
+        System.out.println(Math.round(Math.random() * 100));
+
+        //funções trigonométricas
+        System.out.println(Math.sin(Math.toRadians(30)));
+        System.out.println(Math.cos(Math.toRadians(1)));
+        System.out.println(Math.tan(Math.toRadians(45)));
+    }
+}
